e9b90d1ec1ac4227a82cf8723f04da504887ddf0
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2018-10-05 18:11:31 +0200

    Created j1Player (both .cpp and .h)

---
+++ The Cliffborn Islands/Motor2D/j1Player.cpp
@@ -0,0 +1,18 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Player.h"
+
+j1Player::j1Player() {}
+
+j1Player::~j1Player() {}
+
+//Load assets
+/*bool j1Player::Start() {
+	return true;
+}
+
+bool j1Player::CleanUp() {
+	LOG("Freeing the player");
+	return true;
+}*/
