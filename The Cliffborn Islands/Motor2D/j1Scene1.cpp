b558b7ee01515991b0371c1870e24a7fb1ae721d
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2018-10-10 19:11:55 +0200

    fixed camera bug

--- The Cliffborn Islands/Motor2D/j1Scene1.cpp
+++ The Cliffborn Islands/Motor2D/j1Scene1.cpp
@@ -71,8 +71,14 @@ bool j1Scene1::Update(float dt)
 		App->render->camera.x -= 5;
 
 	// Camera control
-	if ((App->player->position.x > 140) && (App->render->camera.x > -8570))
+	if (App->render->camera.x > -8570)
+	{
 		App->render->camera.x = -App->player->position.x * 4 + 400;
+		if (App->render->camera.x > 0)
+			App->render->camera.x = 0;
+	}
+
+		
 	
 	// Limit player X position
 	if (App->player->position.x > 2374)
