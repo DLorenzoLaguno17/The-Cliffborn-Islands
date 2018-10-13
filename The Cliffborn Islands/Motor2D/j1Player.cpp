ef079af3cbdf7f18979d5e9f8e6845658c171332
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2018-10-13 13:52:02 +0200

    Finished lvl2, colliders included

--- The Cliffborn Islands/Motor2D/j1Player.cpp
+++ The Cliffborn Islands/Motor2D/j1Player.cpp
@@ -324,7 +324,6 @@ bool j1Player::CleanUp() {
 // Detects collision with a wall. If so, the player cannot go further
 void j1Player::OnCollision(Collider* col_1, Collider* col_2)
 {
-
 	if (col_1->type == COLLIDER_PLAYER || col_1->type == COLLIDER_NONE || col_1->type == COLLIDER_FUTURE
 		|| col_2->type == COLLIDER_PLAYER || col_2->type == COLLIDER_NONE || col_2->type == COLLIDER_FUTURE)
 	{
